590c8689edf0f1e8cce75c37414f9923052787ce
kratikakg <132834070+kratikakg@users.noreply.github.com>
2025-06-11 17:22:45 +0530

    mongoose setup

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express= require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to VibeLog API');
+});
+
+const uri= process.env.MONGO_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");    
+}
+);
